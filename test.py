fb781d17652c68af6a052f7dfc7e7228001b5b37
piggy2303 <nguyentuananh23031997@gmail.com>
2019-08-27 00:30:33 +0700

    update

--- test.py
+++ test.py
@@ -2,14 +2,14 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with open("text.txt", "a") as text_file:
-    for i in range(57, 67):
+with open("result_dungphung_leadway.txt", "a") as text_file:
+    for i in range(128, 210):
         voice_file = "Ghi âm của tôi " + str(i) + ".wav"
-        with sr.WavFile("./DATA/" + voice_file) as source:
+        with sr.WavFile("./DATA/dungphung/leadway/" + voice_file) as source:
             audio = r.record(source)
             try:
                 text_voice = r.recognize_google(audio, None, "vi-VN" "en-US")
-                text_file.write(voice_file + " " + str(text_voice) + "\n")
-                print("Trans" + text_voice)
+                text_file.write(text_voice.lower() + "\n")
+                print(voice_file + " " + text_voice)
             except LookupError:
                 print("Could not understand audio")
