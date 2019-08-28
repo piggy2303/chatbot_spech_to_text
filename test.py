f38ae7a4a10eee254a7c13faeaaa77faf9355d08
piggy2303 <nguyentuananh23031997@gmail.com>
2019-08-28 13:23:00 +0700

    update

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with open("result_dungphung_leadway.txt", "a") as text_file:
-    for i in range(128, 210):
+with open("result_minhanh_greeting.txt", "a") as text_file:
+    for i in range(1, 49):
         voice_file = "Ghi âm của tôi " + str(i) + ".wav"
-        with sr.WavFile("./DATA/dungphung/leadway/" + voice_file) as source:
+        with sr.WavFile("./DATA/minhanh/greeting/" + voice_file) as source:
             audio = r.record(source)
             try:
                 text_voice = r.recognize_google(audio, None, "vi-VN" "en-US")
